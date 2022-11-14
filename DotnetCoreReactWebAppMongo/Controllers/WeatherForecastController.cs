d0e93eb4f46bb9b8753cc29d0707e1b1b034b198
Chandra Shekhar <chandra.pandey@winning.com.au>
2022-11-14 16:06:29 +1100

    Initial local Push

---
+++ DotnetCoreReactWebAppMongo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using DotnetCoreReactWebAppMongo.Models;
+
+namespace DotnetCoreReactWebAppMongo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private IMongoCollection<Shipwrecks> _shipwreckCollection;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(IMongoClient client)
+        {
+            var database = client.GetDatabase("sample_geospatial");
+            _shipwreckCollection = database.GetCollection<Shipwrecks>("shipwrecks");
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Shipwrecks> Get()
+        {
+
+            return _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+        }
+    }
+
+    internal interface IMongoCollection
+    {
+    }
+}
+
---
+++ DotnetCoreReactWebAppMongo/Models/Shipwrecks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DotnetCoreReactWebAppMongo.Models
+{
+	[BsonIgnoreExtraElements]
+	public class Shipwrecks
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+
+		[BsonElement("feature_type")]
+		public string FeatureType { get; set; }
+
+		[BsonElement("chart")]
+		public string Chart { get; set; }
+
+		[BsonElement("latdec")]
+		public double Latitude { get; set; }
+
+        [BsonElement("londec")]
+        public double longitude { get; set; }
+
+		//[BsonExtraElements]
+		//public object[] Bucket { get; set; }
+
+	}
+}
+
